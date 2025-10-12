01b3428eabfc3feb300169f5c3582c6e1fdc46c8
nurad9689 <nurad9689@rambler.ru>
2025-10-12 23:28:30 +0300

    corrections

--- database.sql
+++ database.sql
@@ -1,62 +1,73 @@
 --1 step
-CREATE TABLE IF NOT EXISTS Lessons (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    lesson_name VARCHAR(255),
-    lesson_content VARCHAR(255),
-    video_link VARCHAR(255),
-    lesson_possition INTEGER,
-    careated_at DATE,
-    updated_at DATE,
-    is_deleted INT default 0
-);
-
 CREATE TABLE IF NOT EXISTS Courses (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    module_id BIGINT REFERENCES Lessons(id),
-    course_name VARCHAR(255),
-    course_content VARCHAR(255),
+    name VARCHAR(255),
+    description VARCHAR(255),
     created_at DATE,
     updated_at DATE,
-    is_deleted INT default 0
+    deleted_at INT default 0
 );
 
 CREATE TABLE IF NOT EXISTS Modules (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    course_id BIGINT REFERENCES Courses(id),
-    modul_name VARCHAR(255),
-    module_content VARCHAR(255),
+    name VARCHAR(255),
+    description VARCHAR(255),
     created_at DATE,
     updated_at DATE,
-    is_deleted INT default 0
+    deleted_at INT default 0
+);
+
+CREATE TABLE IF NOT EXISTS Course_modules (
+    course_id REFERENCES Courses(id),
+    module_id REFERENCES Modules(id)
 );
 
 CREATE TABLE IF NOT EXISTS Programs (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    module_id BIGINT REFERENCES Modules(id),
-    program_name VARCHAR(255),
+    name VARCHAR(255),
+    price NUMERIC,
     program_type VARCHAR(255),
     created_at DATE,
     updated_at DATE
 );
 
---2 step
-CREATE TYPE role AS ENUM ('student', 'teacher', 'admin');
+CREATE TABLE IF NOT EXISTS Program_modules (
+    program_id REFERENCES Programs(id),
+    module_id REFERENCES Modules(id)
+);
 
-CREATE TABLE IF NOT EXISTS TeachingGroups (
+CREATE TABLE IF NOT EXISTS Lessons (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    groups role,
-    careated_at DATE,
+    course_id BIGINT REFERENCES Courses(id),
+    name VARCHAR(255),
+    description VARCHAR(255),
+    video_url VARCHAR(255),
+    possition INTEGER,
+    created_at DATE,
+    updated_at DATE,
+    deleted_at INT default 0
+);
+
+--2 step
+CREATE TYPE role_status AS ENUM ('student', 'teacher', 'admin');
+
+CREATE TABLE IF NOT EXISTS Teaching_groups (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    slug role,
+    created_at DATE,
     updated_at DATE    
 );
 
 CREATE TABLE IF NOT EXISTS Users (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    user_name VARCHAR(255),
+    name VARCHAR(255),
     email VARCHAR(255),
-    password VARCHAR(255),
-    groups_id BIGINT REFERENCES TeachingGroups(id),
-    careated_at DATE,
-    updated_at DATE    
+    password_hash VARCHAR(255),
+    teaching_group_id BIGINT REFERENCES Teaching_groups(id),
+    role role_status,
+    created_at DATE,
+    updated_at DATE,
+    deleted_at INT default 0
 );
 
 --3 step
@@ -65,9 +76,9 @@ CREATE TYPE enrollment_status AS ENUM ('active', 'pending', 'cancelled', 'comple
 CREATE TABLE IF NOT EXISTS Enrollments (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id BIGINT REFERENCES Users(id),
-    prorgam_id BIGINT REFERENCES Programs(id),
+    program_id BIGINT REFERENCES Programs(id),
     status enrollment_status,
-    careated_at DATE,
+    created_at DATE,
     updated_at DATE    
 );
 
@@ -76,33 +87,33 @@ CREATE TYPE payment_status AS ENUM ('pending', 'paid', 'failed', 'refunded');
 CREATE TABLE IF NOT EXISTS Payments (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     enrollment_id BIGINT REFERENCES Enrollments(id),
-    paymant NUMERIC(255),
+    amount NUMERIC(255),
     status payment_status,
-    payment_date DATE,
-    careated_at DATE,
+    paid_at DATE,
+    created_at DATE,
     updated_at DATE    
 );
 
 CREATE TYPE completions_status AS ENUM ('active', 'completed', 'pending', 'cancelled');
 
-CREATE TABLE IF NOT EXISTS ProgramCompletions (
+CREATE TABLE IF NOT EXISTS Program_completions  (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id BIGINT REFERENCES Users(id),
-    prorgam_id BIGINT REFERENCES Programs(id),
+    program_id BIGINT REFERENCES Programs(id),
     status completions_status,
-    starting_date DATE,
-    finishing_date DATE,
-    careated_at DATE,
+    started_at DATE,
+    completed_at DATE,
+    created_at DATE,
     updated_at DATE    
 );
 
 CREATE TABLE IF NOT EXISTS Certificates (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id BIGINT REFERENCES Users(id),
-    prorgam_id BIGINT REFERENCES Programs(id),
-    certificate_url_link VARCHAR(255),
-    certificate_date DATE,
-    careated_at DATE,
+    program_id BIGINT REFERENCES Programs(id),
+    url VARCHAR(255),
+    issued_at DATE,
+    created_at DATE,
     updated_at DATE    
 );
 
@@ -110,19 +121,18 @@ CREATE TABLE IF NOT EXISTS Certificates (
 CREATE TABLE IF NOT EXISTS Quizzes (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     lesson_id BIGINT REFERENCES Lessons(id),
-    title VARCHAR(255),
-    description TEXT,
-    parent_id BIGINT REFERENCES Quizzes(id),
-    careated_at DATE,
+    name VARCHAR(255),
+    content TEXT,
+    created_at DATE,
     updated_at DATE
 );
 
 CREATE TABLE IF NOT EXISTS Exercises (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     lesson_id BIGINT REFERENCES Lessons(id),
-    title VARCHAR(255),
+    name VARCHAR(255),
     url VARCHAR(255),
-    careated_at DATE,
+    created_at DATE,
     updated_at DATE    
 );
 
@@ -130,20 +140,20 @@ CREATE TABLE IF NOT EXISTS Exercises (
 CREATE TABLE IF NOT EXISTS Discussions (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     lesson_id BIGINT REFERENCES Lessons(id),
-    parent_id BIGINT REFERENCES Discussions(id),
-    description TEXT,
-    careated_at DATE,
+    user_id BIGINT REFERENCES Users(id),
+    text TEXT,
+    created_at DATE,
     updated_at DATE    
 );
 
 CREATE TYPE blog_status AS ENUM ('created', 'in moderation', 'published', 'archived');
 
-CREATE TABLE IF NOT EXISTS Blog (
+CREATE TABLE IF NOT EXISTS Blogs (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id BIGINT REFERENCES Users(id),
-    title VARCHAR(255),
-    description TEXT,
+    name VARCHAR(255),
+    content TEXT,
     status blog_status,
-    careated_at DATE,
+    created_at DATE,
     updated_at DATE    
 );
