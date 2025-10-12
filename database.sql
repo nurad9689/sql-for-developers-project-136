994cbe207882ab7484ba43de86487c59d3430589
nurad9689 <nurad9689@rambler.ru>
2025-10-12 21:46:11 +0300

    step-3

--- database.sql
+++ database.sql
@@ -38,3 +38,70 @@ CREATE TABLE IF NOT EXISTS Programs (
     created_at DATE,
     updated_at DATE
 );
+
+--2 step
+CREATE TYPE role AS ENUM ('student', 'teacher', 'admin');
+
+CREATE TABLE IF NOT EXISTS TeachingGroups (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    groups role,
+    careated_at DATE,
+    updated_at DATE    
+);
+
+CREATE TABLE IF NOT EXISTS Users (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    groups_id BIGINT REFERENCES TeachingGroups(id),
+    careated_at DATE,
+    updated_at DATE    
+);
+
+--3 step
+CREATE TYPE enrollment_status AS ENUM ('active', 'pending', 'cancelled', 'completed');
+
+CREATE TABLE IF NOT EXISTS Enrollments (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT REFERENCES Users(id),
+    prorgam_id BIGINT REFERENCES Programs(id),
+    status enrollment_status,
+    careated_at DATE,
+    updated_at DATE    
+);
+
+CREATE TYPE payment_status AS ENUM ('pending', 'paid', 'failed', 'refunded');
+
+CREATE TABLE IF NOT EXISTS Payments (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    enrollment_id BIGINT REFERENCES Enrollments(id),
+    paymant NUMERIC(255),
+    status payment_status,
+    payment_date DATE,
+    careated_at DATE,
+    updated_at DATE    
+);
+
+CREATE TYPE completions_status AS ENUM ('active', 'completed', 'pending', 'cancelled');
+
+CREATE TABLE IF NOT EXISTS ProgramCompletions (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT REFERENCES Users(id),
+    prorgam_id BIGINT REFERENCES Programs(id),
+    status completions_status,
+    starting_date DATE,
+    finishing_date DATE,
+    careated_at DATE,
+    updated_at DATE    
+);
+
+CREATE TABLE IF NOT EXISTS Certificates (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT REFERENCES Users(id),
+    prorgam_id BIGINT REFERENCES Programs(id),
+    certificate_url_link VARCHAR(255),
+    certificate_date DATE,
+    careated_at DATE,
+    updated_at DATE    
+);
